74ae320953294f5c4a6fee1e386a6e17ccaef32d
fuatefeozdemir <24181617006@gazi.edu.tr>
2026-05-02 15:10:30 +0300

    Setup RouteEngine project structure

---
+++ RouteEngine/API/EngineAPI.cpp
@@ -0,0 +1,5 @@
+//
+// Created by PC on 2.05.2026.
+//
+
+#include "EngineAPI.h"
---
+++ RouteEngine/API/EngineAPI.h
@@ -0,0 +1,16 @@
+//
+// Created by PC on 2.05.2026.
+//
+
+#ifndef ENGINEAPI_H
+#define ENGINEAPI_H
+
+
+
+class EngineAPI {
+
+};
+
+
+
+#endif //ENGINEAPI_H
